ca3141cad655fa17f91f12b71f2ae552db5837dc
kalumba Samuel <kalumbasml@gmail.com>
2023-03-11 05:59:22 +0300

    forms

--- myapp.py
+++ myapp.py
@@ -1,10 +1,19 @@
 from flask import Flask, render_template
+from flask_wtf import Flaskform
+from wtforms import StringField
+from wtforms.validators import DataRequired, Email
 
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/<name>')
+class MyForm(Flaskform):
+    myfield = StringField("Email", validators=[DataRequired(), Email()])
+
+
+@app.route('/<name>', methods=["GET", "POST"])
 def hello(name=None):
+    my_form = MyForm()
+    if my_form.validate_on_submit():
+        mydata = my_form.my_field.data
     return render_template('index.html', name=name)
 
 if __name__ == '__main__':
